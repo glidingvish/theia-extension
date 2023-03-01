29f11419644f238c461e92f293578745128b6343
vishal ghosh <vishal.ghosh28@gmail.com>
2023-03-01 14:12:41 +0530

    added on start hook to open minubrowser and terminal when editor starts

--- hello-world/src/browser/hello-world-contribution.ts
+++ hello-world/src/browser/hello-world-contribution.ts
@@ -1,8 +1,9 @@
 import { injectable, inject } from '@theia/core/shared/inversify';
-import { Command, CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry, MessageService } from '@theia/core/lib/common';
+import { Command, CommandContribution, CommandRegistry, CommandService, MenuContribution, MenuModelRegistry, MessageService } from '@theia/core/lib/common';
 import {TerminalService} from '@theia/terminal/lib/browser/base/terminal-service';
 import {MiniBrowserCommands} from '@theia/mini-browser/lib/browser/mini-browser-open-handler'
-import { CommonMenus } from '@theia/core/lib/browser';
+import { CommonMenus, FrontendApplication, FrontendApplicationContribution } from '@theia/core/lib/browser';
+import { FrontendApplicationStateService } from '@theia/core/lib/browser/frontend-application-state';
 export const HelloWorldCommand: Command = {
     id: 'HelloWorld.command',
     label: 'Run App'
@@ -42,4 +43,52 @@ export class HelloWorldMenuContribution implements MenuContribution {
             label: HelloWorldCommand.label
         });
     }
+}
+@injectable()
+export class HelloWorldFrontendApplicationContribution implements FrontendApplicationContribution {
+    @inject(TerminalService) private readonly terminalService: TerminalService;
+    @inject(MessageService) private readonly messageService: MessageService;
+    @inject(CommandService) private readonly commandService: CommandService;
+    @inject(FrontendApplicationStateService) protected readonly stateService: FrontendApplicationStateService;
+    // async initializeLayout(): Promise<void> {
+    //     console.log("************** initializing ************************");
+    //      // Open a terminal in the bottom panel
+    //     const terminal = await this.terminalService.newTerminal({title:"some command will run here"});
+    //     terminal.start();
+    //     this.terminalService.open(terminal);
+    //     terminal.sendText("echo 'hi glider.ai'\n");
+    //     this.messageService.showProgress({text:"Loading your app",options:{cancelable:true,timeout:5}});
+    // }
+    async onStart(app: FrontendApplication): Promise<void> {
+        this.stateService.reachedState('ready').then(
+            async () => {
+                // console.log("+++++++++++ on state ready ++++++++++++++");
+                // const terminal = await this.terminalService.newTerminal({title:"some command will run here"});
+                // await terminal.start();
+                // this.terminalService.open(terminal);
+                // terminal.sendText("echo 'hi glider.ai'\n");
+                // this.messageService.showProgress({text:"Loading your app",options:{cancelable:true,timeout:5}});
+                this.commandService.executeCommand(HelloWorldCommand.id);
+            }
+        );
+    }
+}
+
+@injectable()
+export class HelloworldFrontendApplication extends FrontendApplication {
+
+    @inject(TerminalService) private readonly terminalService: TerminalService;
+    @inject(MessageService) private readonly messageService: MessageService;
+    protected async createDefaultLayout(): Promise<void> {
+        // Initialize the default layout for search-in-workspace, git, etc.
+        await super.createDefaultLayout();
+        console.log("************** hooking ************************");
+        this.messageService.showProgress({text:"hook Loading your app",options:{cancelable:true,timeout:5}});
+        // Open a terminal in the bottom panel
+        const terminal = await this.terminalService.newTerminal({});
+        terminal.start();
+        this.terminalService.open(terminal);
+        terminal.sendText("echo 'hi glider.ai'\n");
+    }
+
 }
--- hello-world/src/browser/hello-world-frontend-module.ts
+++ hello-world/src/browser/hello-world-frontend-module.ts
@@ -1,12 +1,14 @@
 /**
  * Generated using theia-extension-generator
  */
-import { HelloWorldCommandContribution, HelloWorldMenuContribution } from './hello-world-contribution';
-import { CommandContribution, MenuContribution } from '@theia/core/lib/common';
+import { CommandContribution } from '@theia/core';
+import { FrontendApplicationContribution } from '@theia/core/lib/browser';
 import { ContainerModule } from '@theia/core/shared/inversify';
+import { HelloWorldCommandContribution, HelloWorldFrontendApplicationContribution } from './hello-world-contribution';
 
-export default new ContainerModule(bind => {
+export default new ContainerModule((bind, unbind, isBound, rebind) => {
     // add your contribution bindings here
     bind(CommandContribution).to(HelloWorldCommandContribution);
-    bind(MenuContribution).to(HelloWorldMenuContribution);
+    // bind(MenuContribution).to(HelloWorldMenuContribution);
+    bind(FrontendApplicationContribution).to(HelloWorldFrontendApplicationContribution);
 });
